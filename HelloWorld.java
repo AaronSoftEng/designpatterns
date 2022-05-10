6cc33f4cd9e4d0277651f312e99560e7e4d3480f
AaronSoftEng <alkeys@email.sc.edu>
2022-05-10 13:11:03 -0400

    Added new classes via the uml spec

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,11 @@
+/*
+ * using unix
+ * git clone url
+ *
+ * git add .
+ * git commit -m "adding fun.txt"
+ * git push origin main
+ */
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
